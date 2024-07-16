641d8adea4653334c8b0fbe157467210c5fff64b
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-07-16 16:05:21 +0530

    implemented some utils funtions

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,7 @@
+
+
+export const asyncHandler = (passedFunction)=> {
+    return (req, res, next)=> {
+        Promise.resolve(passedFunction(req, res, next)).catch(next);
+    }
+}
---
+++ src/utils/index.js
@@ -0,0 +1,10 @@
+import { ApiResponse } from "./ApiResponse.js";
+import {ApiError} from './ApiError.js';
+import {asyncHandler} from './asyncHandler.js';
+
+
+export{
+    ApiError,
+    ApiResponse,
+    asyncHandler
+}
