272f55e5e16492d2a9b450f4ca104dc9f0c9e3dd
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-07-17 11:11:18 +0530

    implemented controller for update avatar and cover image

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -201,6 +201,55 @@ const updateAccountDeatails = asyncHandler(async(req, res, next)=> {
 })
 
 
+const updateUserAvatar = asyncHandler(async(req, res, next)=> {
+     const avatarLocalPath = req.file?.path;
+     if(!avatarLocalPath) {
+        throw new ApiError(400, "avatar file is required");
+     }
+
+     const avatar = await uploadOnCloudinary(avatarLocalPath);
+     if(!avatar.url) {
+        throw new ApiError(400, "Error while uploading avatar on cloudinary");
+     }
+
+     await User.findByIdAndUpdate(
+        req.user._id,
+        {$set:{avatar:avatar.url}},
+        {new:true}
+     ).select('-password -refreshToken');
+
+     return res.status(200)
+     .json(new ApiResponse(200, {}, "Avatar updated"));
+
+
+})
+
+
+const updateUserCoverImage = asyncHandler(async(req, res, next)=> {
+    const coverImageLocalPath = req.file?.path;
+    if(!coverImageLocalPath) {
+       throw new ApiError(400, "avatar file is required");
+    }
+
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+    if(!coverImage.url) {
+       throw new ApiError(400, "Error while uploading avatar on cloudinary");
+    }
+
+    await User.findByIdAndUpdate(
+       req.user._id,
+       {$set:{coverImage:coverImage.url}},
+       {new:true}
+    ).select('-password -refreshToken');
+
+    return res.status(200)
+    .json(new ApiResponse(200, {}, "Cover image updated"));
+
+
+})
+
+
+
 export {
     registerUser,
     loginUser,
@@ -208,5 +257,7 @@ export {
     refreshAccessToken,
     changeCurrentPassword,
     getCurrentUser,
-    updateAccountDeatails
+    updateAccountDeatails,
+    updateUserAvatar,
+    updateUserCoverImage
 }
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -2,7 +2,7 @@ import express from  'express';
 const router = express.Router();
 import {upload} from '../middleware/multer.middleware.js';
 import {verifyJwt} from '../middleware/auth.middleware.js'
-import {registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDeatails} from '../controller/user.controller.js'
+import {registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDeatails, updateUserAvatar, updateUserCoverImage} from '../controller/user.controller.js'
 
 router.post('/register', upload.fields([
     {
@@ -22,7 +22,9 @@ router.post('/logout', verifyJwt, logoutUser);
 router.post('/refresh-accessToken', verifyJwt, refreshAccessToken);
 router.post('/change-password', verifyJwt, changeCurrentPassword);
 router.get('/getCurrent-user',verifyJwt,  getCurrentUser);
-router.post('/update-accountDetails', verifyJwt, updateAccountDeatails);
+router.put('/update-accountDetails', verifyJwt, updateAccountDeatails);
+router.put('/update-avatar', verifyJwt, upload.single("avatar"), updateUserAvatar);
+router.put('/update-coverImage', verifyJwt, upload.single("coverImage"), updateUserCoverImage);
 
 
 export default router;
